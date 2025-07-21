571b6c0f1c02a6463941e10d073ba21448d5421c
Mr-Clever1 <90503850+Mr-Clever1@users.noreply.github.com>
2025-07-22 09:49:35 +1200

    Add files via upload

--- SchematicDrawer.py
+++ SchematicDrawer.py
@@ -3,12 +3,15 @@
 #James Burt
 from tkinter import *
 
+CANVAS_SIZE = (437, 614)
 IMAGE_FOLDER_PATH = r"Toolbar_Icons/"
-TOOLBAR_BUTTONS = {
-    "RECTANGLE":"BUTTON_1.png",
-    "ELIPSE":"BUTTON_2.png",
-    "TRIANGLE":"BUTTON_3.png",
-    "LINE":"BUTTON_4.png"
+TOOLBAR_LAYOUT = {
+    "CONFIGURE":"SETTINGS.png",
+    "PRINT":"PRINT.png",
+    "RECTANGLE":"RECTANGLE.png",
+    "ELIPSE":"ELIPSE.png",
+    "TRIANGLE":"TRIANGLE.png",
+    "LINE":"LIne.png"
 }
 
 #Stores all of the information I require about each polygon on the canvas
@@ -23,9 +26,14 @@ class Shape:
 
 class Drawer:
     def __init__(self) -> None:
-        self.current_shape = 0
+        #Establish basic shape information
+        self.current_shape = None
+        self.drawing_type = "RECTANGLE"
+        self.current_vertices = []
+
+
         self.root = self.createMainWindow()
-        self.place_buttons(TOOLBAR_BUTTONS,self.toolbar_frame)
+        self.place_buttons(TOOLBAR_LAYOUT,self.toolbar_frame)
 
         pass
 
@@ -47,19 +55,39 @@ class Drawer:
         self.design_frame = Frame(root,bg="red")
         self.design_frame.grid(row=2,column=0,sticky="news")
         
-
-
+        #Establish the Tkinter canvas for drawing on
+        self.design = Canvas(self.design_frame,width=CANVAS_SIZE[0],height=CANVAS_SIZE[1],bg="white")
+        self.design.grid(row=1,column=0)
         return root
 
     def mouseDown(self,event):
-        pass
+        if self.drawing_type != "":
+            self.mouse_down_event = event
+            pass
 
     def mouseUp(self,event):
+        self.mouse_up_event = event
+        self.design.create_polygon(self.current_vertices, outline='black', fill='', dash=(4, 2))
         pass    
     
     def mouseDrag(self,event):
-        pass
+         if self.drawing_type != "":
+            print("BLEH")
+            self.mouse_pos = (event.x,event.y)
+            x0,y0 = (self.mouse_down_event.x,self.mouse_down_event.y)
+            x1,y1 = self.mouse_pos
+            shape_vertices = []
+            match(self.drawing_type):
+                case "RECTANGLE":
+                    shape_vertices = [x0,y0,x1,y0,x1,y1,x0,y1]
+                case "TRIANGLE":
+                    shape_vertices = [x0,y0,x1,y1,x0,y1]
+            
+            self.current_vertices = shape_vertices
+                    
+                    
 
+                
     def place_buttons(self,to_be_placed:dict,frame:Frame):
         self.labels = []
         for index,key in enumerate(to_be_placed):
@@ -68,6 +96,8 @@ class Drawer:
             label = Button(frame,image=to_be_placed[key],command=None)
             label.grid(row=0,column=index)
             self.labels.append(label)
+            
+
             
             
 
