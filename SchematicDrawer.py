b7fabdcd0c87ab4c3f72fbf3238a2dcc9d1975c5
Mr-Clever1 <90503850+Mr-Clever1@users.noreply.github.com>
2025-07-25 10:48:44 +1200

    25/07

--- SchematicDrawer.py
+++ SchematicDrawer.py
@@ -19,6 +19,22 @@ TOOLBAR_LAYOUT = {
     "LINE":"LIne.png"
 }
 
+#Paper Sizes, sizes in pixles then mm
+AN_SIZES = {
+    0: [(9933,14043),(841, 1189)],
+    1: [(7016,9933),(594, 841)],
+    2: [(4961,7016),(420, 594)],
+    3: [(3508,4961), (297, 420)],
+    4: [(2480,3508),(210, 297)],
+    5: [(1748,2480),(148, 210)],
+    6: [(1240,1748),(105, 148)],
+    7: [(874,1240),(74, 105)],
+    8: [(614,874),(52, 74)],
+    9: [(437,614), (37, 52)],
+    10:[(307,437),(26, 37)]
+
+}
+
 #Stores all of the information I require about each polygon on the canvas
 class Shape:
     def __init__(self,type,vertices,rect,colour) -> None:
@@ -37,6 +53,7 @@ class Drawer:
         self.current_vertices = []
         self.is_drawing = False
         self.temp_shape = None
+        self.current_paper_size = 5
 
         self.all_polygons = []
         self.all_coordinates = []
@@ -102,12 +119,12 @@ class Drawer:
             rect = self.design.create_polygon(*self.current_vertices, outline='black', fill='')
             print(rect)
             #Create shape info to store
-            new_shape = Shape(self.drawing_type,self.convert_coordinates(self.current_vertices),rect,"None")
+            new_shape = Shape(self.drawing_type,self.convert_coordinates_format(self.current_vertices),rect,"None")
             self.all_polygons.append(new_shape)
             pass    
     #Called when mouse is moved over the canvas
     def mouseDrag(self,event):
-        self.mouse_pos = (event.x,event.y)
+        self.mouse_pos = (self.clamp(event.x,0,CANVAS_SIZE[0]),self.clamp(event.y,0,CANVAS_SIZE[1]))
         if self.is_drawing == True:
             x0,y0 = (self.mouse_down_event.x,self.mouse_down_event.y)
             x1,y1 = self.mouse_pos
@@ -136,19 +153,32 @@ class Drawer:
             self.all_buttons.append(Button(frame,image=to_be_placed[key],command=lambda k=key:self.button_commands(k)))
             self.all_buttons[-1].grid(row=0,column=index)
     def update_dimensions(self):
-        x0,y0 = self.mouse_pos
+        #Unrounded mouse pos
+        ur_x0,ury0 = self.convert_point_to_mm(self.mouse_pos)
+        x0,y0 = self.convert_point_to_mm(self.mouse_pos)
         text = f"Position(mm): {(x0,y0)} "
         if self.is_drawing == True:
-            x1,y1 = (self.mouse_down_event.x,self.mouse_down_event.y)
-            xy_scale = (y0-y1,y0-y1)
-            text = text+f"Scale(mm): {(x0-x1,y0-y1)} "
+            #Unrounded mouse pos
+            start_mouse_pos = (self.mouse_down_event.x,self.mouse_down_event.y)
+            ur_x1,ury1 = self.convert_point_to_mm(start_mouse_pos)
+            x1,y1 = self.convert_point_to_mm(start_mouse_pos)
+            xy_scale = (ur_x0-ur_x1,ury0-ury1)
+            text = text+f"Scale(mm): {(round(x0-x1,2),round(y0-y1,2))} "
             if self.drawing_type == "LINE":
-                text = text+f"Length(mm): {math.sqrt((xy_scale[0]**2)+(xy_scale[1]**2))}"
+                text = text+f"Length(mm): {round(math.sqrt((xy_scale[0]**2)+(xy_scale[1]**2)),2)}"
         self.dimension_val.set(text)
     
+    def convert_point_to_mm(self,point):
+        mm_size = AN_SIZES[self.current_paper_size][1]
 
+        pxpmm_x = CANVAS_SIZE[0]/mm_size[0]
+        pxpmm_y = CANVAS_SIZE[1]/mm_size[1]
+        #Converts pixel coord to mm
+        mm_point = (point[0]/pxpmm_x,point[1]/pxpmm_y)
+        mm_point = (self.clamp(mm_point[0],0,mm_size[0]),self.clamp(mm_point[1],0,mm_size[1]))
+        return mm_point
     #Converts coordinates from each value being its own item to tuples of coords
-    def convert_coordinates(self,inital_coords):
+    def convert_coordinates_format(self,inital_coords):
         #tkinter likes each value as its own item in a list
         #PIL likes tuples of coordinates
         tuple_coords = []
@@ -158,8 +188,13 @@ class Drawer:
 
     def button_commands(self,command):
         match(command):
+            case "CONFIGURE":
+                pass
             case _:
                 self.drawing_type = command
+
+    def clamp(self,val,lower,upper):
+        return round(max(lower,min(val,upper)),2)
 
             
             
