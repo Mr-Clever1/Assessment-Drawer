2bdd84ef1128f0aa5f56edca8198221570657fa4
Mr-Clever1 <90503850+Mr-Clever1@users.noreply.github.com>
2025-08-07 12:39:51 +1200

    Working on deleting functionality, currently non functional as i am more concerned about selecting the right shape than deleting it.

--- SchematicDrawer.py
+++ SchematicDrawer.py
@@ -5,6 +5,7 @@ from tkinter import *
 import RedrawerComponent 
 import MeasurementComponent 
 import ConfigurationComponent
+import math
 #Define program constants
 
 #Defines image names and folder that they're in
@@ -15,18 +16,21 @@ TOOLBAR_LAYOUT = {
     "RECTANGLE":"RECTANGLE.png",
     "ELIPSE":"ELIPSE.png",
     "TRIANGLE":"TRIANGLE.png",
-    "LINE":"LIne.png"
+    "LINE":"LINE.png",
+    "DELETE":"DELETE.png"
+
 }
 
 
 
 #Stores all of the information I require about each polygon on the canvas
 class Shape:
-    def __init__(self,type,vertices,rect,colour) -> None:
+    def __init__(self,type,vertices,rect,colour,area) -> None:
         self.type = type
         self.vertices = vertices
         self.rect = rect
         self.colour = colour
+        self.area = area
         pass
 
 
@@ -37,12 +41,12 @@ class Drawer:
         self.drawing_type = ""
         self.current_vertices = []
         self.is_drawing = False
-        self.temp_shape = None
         self.current_paper_size = 4
         self.real_scale = 100
         self.CONFIG_SCALE = 100
         self.all_polygons = []
         self.all_coordinates = []
+        self.current_area = 0
         #Paper Sizes, sizes in pixles then mm
         self.AN_SIZES = {
             0: [(9933,14043),(841, 1189)],
@@ -109,23 +113,46 @@ class Drawer:
 
     #Called when mouse down
     def mouseDown(self,event):
-        if self.drawing_type != "":
+        print("click")
+        if self.drawing_type != "" and self.drawing_type != "DELETE":
             self.mouse_down_pos = (event.x,event.y)
             self.is_drawing = True
             self.current_vertices = [0,0,0,0,0,0]
             #Create temp shape for design guide
-            self.temp_shape = self.design.create_polygon(*self.current_vertices, outline='black', fill='', dash=(4, 2))
+            self.current_shape = self.design.create_polygon(*self.current_vertices, outline='black', fill='', dash=(4, 2))
             pass
+        elif self.drawing_type == "DELETE":
+
+            # self.find_bounding(event.x,event.y)
+            overlapping = self.design.find_overlapping(event.x, event.y,event.x, event.y)
+            for overlap_shape in overlapping:
+                bounding = self.design.coords(overlap_shape)
+                inside_check = self.design.find_overlapping(bounding[0], bounding[1],bounding[2], bounding[3])
+                for other_shapes in overlapping:
+                    if other_shapes in inside_check:
+                        pass
+                    else:
+                        print(f"Topmost rect is: {other_shapes}")
+
+            # smallest = Shape("RECTANGLE",[0,0,self.CANVAS_SIZE[0],self.CANVAS_SIZE[1]],0,None,math.inf)
+            # for shape in self.all_polygons:
+            #     if shape.rect in overlapping and shape.area < smallest.area:
+            #         smallest = shape
+            # if smallest.area != math.inf:
+            #     self.delete_shape(smallest)
+            #     print("Deleted")
+            
+            
 
     #Called when mouse released
     def mouseUp(self,event):
         if self.is_drawing == True:
             self.mouse_up_event = event
             self.is_drawing = False
-            rect = self.design.create_polygon(*self.current_vertices, outline='black', fill='')
+            self.design.itemconfigure(self.current_shape, outline="black",dash=(), width=1)
             #Create shape info to store
             print(self.convert_coordinates_format(self.current_vertices))
-            new_shape = Shape(self.drawing_type,self.convert_coordinates_format(self.current_vertices),rect,"None")
+            new_shape = Shape(self.drawing_type,self.convert_coordinates_format(self.current_vertices),self.current_shape,"None",self.current_area)
             self.all_polygons.append(new_shape)
             pass    
     #Called when mouse is moved over the canvas
@@ -139,14 +166,30 @@ class Drawer:
             match(self.drawing_type):
                 case "RECTANGLE":
                     shape_vertices = [x0,y0,x1,y0,x1,y1,x0,y1]
+                    self.current_area = abs(x1*y1)
                 case "TRIANGLE":
                     shape_vertices = [x0,y0,x1,y1,x0,y1]
+                    self.current_area = abs(0.5*x1*y1)
                 case "LINE":
                     shape_vertices = [x0,y0,x1,y1]
+                case "ELIPSE":                    
 
+                    shape_vertices = []
+                    center_x = (self.mouse_pos[0]+self.mouse_down_pos[0])/2
+                    center_y = (self.mouse_pos[1]+self.mouse_down_pos[1])/2
+                    radius_x = abs(self.mouse_pos[0]-self.mouse_down_pos[0])/2
+                    radius_y = abs(self.mouse_pos[1]-self.mouse_down_pos[1])/2
+                    self.current_area = abs(radius_y*radius_x*math.pi)
+                    num_points = 30
+                    for i in range(num_points):
+                        angle = 2 * math.pi * i / num_points
+                        x = center_x + radius_x * math.cos(angle)
+                        y = center_y + radius_y * math.sin(angle)
+                        shape_vertices.append(x)
+                        shape_vertices.append(y)
 
             #Resize current shape to be new size
-            self.design.coords(self.temp_shape,*shape_vertices)                    
+            self.design.coords(self.current_shape,*shape_vertices)                    
             self.current_vertices = shape_vertices  
 
         self.dimension_val.set(MeasurementComponent.update_dimensions(self))
@@ -187,8 +230,11 @@ class Drawer:
                     RedrawerComponent.tkinter_to_PIL(self,[config_shape],1)
             case _:
                 self.drawing_type = command
-    
         
+
+    def delete_shape(self,shape):
+        self.all_polygons.remove(shape)
+        self.design.delete(shape.rect)    
     def clamp(self,val,lower,upper):
         return round(max(lower,min(val,upper)),2)
 
