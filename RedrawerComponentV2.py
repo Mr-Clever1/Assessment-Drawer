51c73ccef2154d4610bcc20e4409f03bb24bbe6c
Mr-Clever1 <90503850+Mr-Clever1@users.noreply.github.com>
2025-08-25 13:11:21 +1200

    Reverted Redrawer back to without colour and moved new features into RedrawerV2

--- RedrawerComponentV2.py
+++ RedrawerComponentV2.py
@@ -3,9 +3,14 @@
 #30/07/2025
 #James Burt
 from PIL import Image,ImageDraw
+import win32com.client
+import os
+import threading
+import pythoncom
 
 #Called when attempting to print an image
 def tkinter_to_PIL(self,shapes,scale):
+    global file
     #Creates a new file and canvas
     file = Image.new("RGB",self.canvas_size,"white")
     draw = ImageDraw.Draw(file)
@@ -15,13 +20,23 @@ def tkinter_to_PIL(self,shapes,scale):
             #Creates a adjusts the list item to be scaled correctly
             polygon.vertices[index] = (vertex[0]*scale,vertex[1]*scale)
 
- 
+        if polygon.colour == ("" or "None"):
+            polygon.colour = None
         #Draws onto new canvas
-        draw.polygon(polygon.vertices,outline="black",fill=None)
+        draw.polygon(polygon.vertices,outline="black",fill=polygon.colour)
     #Scales drawing to be desired paper size
     file = file.resize(self.AN_SIZES[self.current_paper_size][0])
+    threading.Thread(target=print_picture,daemon=True).start()
+def print_picture():
 
+    pythoncom.CoInitialize() 
     #Saves and opens image
-    file.save("MyImage.png")
-    file.show()
+    file_name = "RecentPrint.png"
+    file_path = os.path.abspath(os.getcwd())+f"\{file_name}"
+    file.save(file_path)
+    
 
+    wia_dialog = win32com.client.Dispatch("WIA.CommonDialog")
+
+    # Launch the photo printing wizard with the specified file
+    wia_dialog.ShowPhotoPrintingWizard(file_path)
